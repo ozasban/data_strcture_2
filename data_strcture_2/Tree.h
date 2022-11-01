828750afb3e4005509ee83c52d860b5bd38a509a
ozasban <oz6962@gmail.com>
2022-11-01 14:04:20 +0200

    all files

--- data_strcture_2/Tree.h
+++ data_strcture_2/Tree.h
@@ -7,5 +7,15 @@ using namespace std;
 
 class Tree
 {
-
+	Node* discussion;
+public:
+	Tree();
+	~Tree();
+	void buildNewRoot(string title);
+	Node* search(string str);
+	void addResponse( string father, string son);
+	void delSubTree(string str);
+	void printTree();
+	void searchPrintRoot(string str);
+	void searchPrintLeaf(string str);
 };

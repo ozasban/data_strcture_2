b0eda566fba65c6e5bcf872bc6a2daabca4f04d5
ozasban <oz6962@gmail.com>
2022-12-11 15:16:02 +0200

    80%

--- Data_structure_2_exercise_3/trie.cpp
+++ Data_structure_2_exercise_3/trie.cpp
@@ -2,6 +2,13 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+/*
+oz asban 207565607
+binymin shapira 208965863
+data structure 2 exercise 3
+class for trie  .cpp
+*/
+
 /// <summary>
 /// returns new empty nodetrie
 /// </summary>
@@ -9,7 +16,12 @@
 /// <returns></returns>
 TrieNode* getNode(void)
 {   
-    return new TrieNode;
+    TrieNode* tr= new TrieNode;
+    tr->parent = nullptr;
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+        tr->children[i] = nullptr;
+    tr->isWordEnd = false;
+    return tr;
 }
 /// <summary>
 /// return true if node is last letter in woed
@@ -50,8 +62,8 @@ bool insertT(TrieNode* root, string key)
         //else build the word
         else
         {
-            root->children[CHAR_TO_INDEX(key[i])] = new TrieNode;
-            root->children[CHAR_TO_INDEX(key[i])] = root;
+            root->children[CHAR_TO_INDEX(key[i])] = getNode();
+            root->children[CHAR_TO_INDEX(key[i])]->parent = root;
             root = root->children[CHAR_TO_INDEX(key[i])];
             if (i == (key.length() - 1))
             {
@@ -87,7 +99,7 @@ bool delT(TrieNode* root, string key)
     //if the word is exist delete
     if (searchT(root, key))
     {
-        TrieNode* lastL;//get pointer to the last node in the string
+        TrieNode* lastL=getNode();//get pointer to the last node in the string
         for (int i = 0; i < key.length(); i++)
         {
             root = root->children[CHAR_TO_INDEX(key[i])];
@@ -98,17 +110,20 @@ bool delT(TrieNode* root, string key)
        //if the last letter have no childrens delete the node
         if (haveNoChildren(lastL))
         {
-            TrieNode* temp = lastL;
+            //TrieNode* temp = lastL;
             lastL = lastL->parent;
-            temp = nullptr;
+            lastL->children[CHAR_TO_INDEX(key[key.length() - 1])] = nullptr;
+            //temp = nullptr;
+           // delete temp;
             //if the parent have no children do the same
             for (int i = 0; i < key.length() - 1; i++)
             {
                 if (haveNoChildren(lastL))
                 {
-                    TrieNode* temp = lastL;
+                    //TrieNode* temp = lastL;
                     lastL = lastL->parent;
-                    temp = nullptr;
+                    lastL->children[CHAR_TO_INDEX(key[key.length() - i - 2])] = nullptr;
+                    //temp = nullptr;
                 }
                 else
                     break;
@@ -120,6 +135,103 @@ bool delT(TrieNode* root, string key)
         return true;
     }
     
+}
+/// <summary>
+/// return true if the word exist in the tree
+/// </summary>
+/// <param name="root"></param>
+/// <param name="key"></param>
+/// <returns></returns>
+bool searchT(TrieNode* root, string key)
+{
+    //run on all the word
+    for (int i = 0; i < key.length(); i++)
+    {
+        //if sun exist continue
+        if (root->children[CHAR_TO_INDEX(key[i])] != nullptr)
+        {
+            root = root->children[CHAR_TO_INDEX(key[i])];
+            if (i == key.length() - 1)//check if its end of word
+                if (!root->isWordEnd)//if there is no such a word
+                    return false;
+        }
+        //if no such a son exist
+        else
+            return false;
+    }
+    return true;
+}
+/// <summary>
+/// recursive method to print all the sub trees for preword
+/// </summary>
+/// <param name="root"></param>
+/// <param name="currPrefix"></param>
+void suggestionsRec(TrieNode* root, string currPrefix)
+{
+    //if its a leef print the word and return
+    if (haveNoChildren(root)&& root->isWordEnd)
+    {
+        cout << currPrefix << endl;
+        return;
+    }
+    //if it is and of word but not a leef print and continue
+    else if (root->isWordEnd)
+    {
+        cout << currPrefix << endl;
+        for (int i = 0; i < ALPHABET_SIZE; i++)
+        {
+            if (root->children[i] != nullptr)
+                suggestionsRec(root->children[i], currPrefix + INDEX_TO_CHAR(i));
+        }
+    }
+    //if its a midle of a word continue
+    else
+    {
+        for (int i = 0; i < ALPHABET_SIZE; i++)
+        {
+            if (root->children[i] != nullptr)
+                suggestionsRec(root->children[i], currPrefix + INDEX_TO_CHAR(i));
+        }
+    }
+}
+/// <summary>
+/// get start of a string and compleete it
+/// </summary>
+/// <param name="root"></param>
+/// <param name="query"></param>
+/// <returns></returns>
+int printAutoSuggestionsT(TrieNode* root, string query)
+{
+    //find tha last latter to start searching and printing from ther
+    for (int i = 0; i < query.length(); i++)
+    {
+        if (root->children[CHAR_TO_INDEX(query[i])] != nullptr)
+            root = root->children[CHAR_TO_INDEX(query[i])];
+        else
+            return 0;
+    }
+    suggestionsRec(root, query);
+}
+/// <summary>
+/// print all tree in recursion
+/// </summary>
+/// <param name="t"></param>
+/// <param name="level"></param>
+void printT(TrieNode* t, int level)
+{
+    //print all childrens in orde
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+    {
+        //if the son exist print and check hus childrens
+        if (t->children[i] != nullptr)
+        {
+            for (int i = 0; i < level; i++)
+                cout << " ";
+            cout << INDEX_TO_CHAR(i) << ":" << endl;
+            printT(t->children[i], level + 1);
+        }
+    }
+    return;
 }
 /// <summary>
 /// return true if have no childrens
--- Data_structure_2_exercise_3/trie.h
+++ Data_structure_2_exercise_3/trie.h
@@ -4,6 +4,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+/*
+oz asban 207565607
+binymin shapira 208965863
+data structure 2 exercise 3
+class for trie  .h
+*/
 
 // Alphabet size (# of symbols) 
 #define ALPHABET_SIZE (26) 
@@ -11,6 +17,9 @@ using namespace std;
 // Converts key current character into index 
 // use only 'a' through 'z' and lower case 
 #define CHAR_TO_INDEX(c) ((int)c - (int)'a') 
+// Converts childrens current index into character
+// use only 'a' through 'z' and lower case 
+#define INDEX_TO_CHAR(c) ((char)((int)c + (int)'a')) 
 
 // trie node 
 struct TrieNode
@@ -25,16 +34,16 @@ struct TrieNode
 
 
 TrieNode* getNode(void);
-bool isLastNode(TrieNode* root);
-bool insertT(TrieNode* root, string key);
+bool isLastNode(TrieNode* Root);
+bool insertT(TrieNode* Root, string key);
 bool isLowerCase(string key);
-bool delT(TrieNode* root, string key);
-bool delR(TrieNode* prev, TrieNode* root, string key, int ind);
-bool searchT(TrieNode* root, string key);
-void suggestionsRec(TrieNode* root, string currPrefix);
-int printAutoSuggestionsT(TrieNode* root, string query);
+bool delT(TrieNode* Root, string key);
+//bool delR(TrieNode* prev, TrieNode* root, string key, int ind);
+bool searchT(TrieNode* Root, string key);
+void suggestionsRec(TrieNode* Root, string currPrefix);
+int printAutoSuggestionsT(TrieNode* Root, string query);
 void printT(TrieNode* t, int level);
-bool haveNoChildren(TrieNode* root);
+bool haveNoChildren(TrieNode* Root);
 
 
 
